20c8d6095ef550eeb77478951a0d9cb1f725db53
WolfElkan <wolf.elkan@gmail.com>
2018-06-02 13:27:37 -0400

    Deprecate Enrollment.tuition(); Use Enrollment.course.tuition() instead

--- apps/people/models.py
+++ apps/people/models.py
@@ -147,12 +147,12 @@ class Family(models.Model):
 		qset = qset.order_by('created_at')
 		return qset
 	def total_tuition_in(self, year):
-		return sum(Each(self.enrollments_in(year)).tuition)
+		return sum(Each(Each(self.enrollments_in(year)).course).tuition)
 	def paid_tuition_in(self, year):
 		return sum(Each(Invoices.filter(family=self,status='P')).amount)
 		# return sum(collect(self.enrollments_in(year), lambda enr: 0 if enr.isAudition else enr.course.tuition))
 	def pend_tuition_in(self, year):
-		return sum(Each(self.pend_enrollments_in(year)).tuition)
+		return sum(Each(Each(self.pend_enrollments_in(year)).course).tuition)
 		# return sum(Each(Invoices.filter(family=self,status='N')).amount) + sum(Each(Each(Enrollments.filter(student__family=self, course__year=year, isAudition=True, happened=False)).course).tuition)
 	def unpaid_tuition_in(self, year):
 		return self.total_tuition_in(year) - self.pend_tuition_in(year) - self.paid_tuition_in(year)
