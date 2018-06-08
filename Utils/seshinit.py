f5244bfff8f5b5658e842ade833cb8fb283bd344
WolfElkan <wolf.elkan@gmail.com>
2018-06-08 15:54:51 -0400

    Add ability to add new students

--- Utils/seshinit.py
+++ Utils/seshinit.py
@@ -5,13 +5,15 @@ def seshinit(request, sesh, val=''):
 
 # Initialize form error/persist structure in session
 def forminit(request, form_name, fields, obj=None, data=None):
-		if form_name not in request.session['p']:
-			request.session['p'][form_name] = {}
-			for f in fields:
-				if f not in request.session['p'][form_name]:
-					request.session['p'][form_name][f] = obj.__getattribute__(f) if obj and hasattr(obj,f) else data[f] if data and f in data else ''
-		if form_name not in request.session['e']:
-			request.session['e'][form_name] = {}
-			for f in fields:
-				if f not in request.session['e'][form_name]:
-					request.session['e'][form_name][f] = ''
+	seshinit(request,'p',{})
+	seshinit(request,'e',{})
+	if form_name not in request.session['p']:
+		request.session['p'][form_name] = {}
+		for f in fields:
+			if f not in request.session['p'][form_name]:
+				request.session['p'][form_name][f] = obj.__getattribute__(f) if obj and hasattr(obj,f) else data[f] if data and f in data else ''
+	if form_name not in request.session['e']:
+		request.session['e'][form_name] = {}
+		for f in fields:
+			if f not in request.session['e'][form_name]:
+				request.session['e'][form_name][f] = ''
