2500339755bd16ec78b756c147e2162fa4835cf5
WolfElkan <wolf.elkan@gmail.com>
2018-07-12 13:47:45 -0400

    DEBUG variable moved to local specs

--- HST/settings.py
+++ HST/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ignored.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = ignored.DEBUG
 
 CURRENT_HOST = ignored.NGROK_URL
 
